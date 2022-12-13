ffc340a22f742f27fc005688bf4bdd3d186cd3d0
WeiInGit <91000872+WeiInGit@users.noreply.github.com>
2022-12-13 15:16:00 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,27 +7,35 @@ const api = new MiroApi(oauthAccessToken);
 // Get the Miro board using the board ID
 const board = api.getBoard(boardId);
 
-// Initialize the event listener and create a sticky note when the icon is clicked
+// Select the button element on the page
+const button = document.querySelector('#my-button');
+
+// Add an event listener to the button to listen for clicks
+button.addEventListener('click', async () => {
+  // Create a new sticky note on the Miro board when the button is clicked
+  const sticky = await board.createStickyNoteItem({
+    data: {
+      content: "I'm a sticky note",
+      shape: "square",
+    },
+    style: {
+      fillColor: "light_yellow",
+      textAlign: "center",
+      textAlignVertical: "top",
+    },
+    position: {
+      x: 0,
+      y: 0,
+    },
+  });
+
+  // Open a panel in the Miro board UI to display the sticky note
+  await miro.board.ui.openPanel({url: 'app.html'});
+});
+
+// Initialize the Miro board UI event listener
 async function init() {
   miro.board.ui.on('icon:click', async () => {
-    // Create a new sticky note on the Miro board
-    const sticky = await board.createStickyNoteItem({
-      data: {
-        content: "I'm a sticky note",
-        shape: "square",
-      },
-      style: {
-        fillColor: "light_yellow",
-        textAlign: "center",
-        textAlignVertical: "top",
-      },
-      position: {
-        x: 0,
-        y: 0,
-      },
-    });
-
-    // Open a panel in the Miro board UI with the specified HTML page
     await miro.board.ui.openPanel({url: 'app.html'});
   });
 }
